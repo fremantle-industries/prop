1ecc99355308ec43c91461bdfb653f2023e2f438
alex <alex+git@fremantle.io>
2021-08-31 14:17:02 -0700

    Release v0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Prop.MixProject do
   def project do
     [
       app: :prop,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.12",
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
