fb856082528149c6b8dd74664978aee38f379a63
alex <alex+git@fremantle.io>
2021-12-11 13:51:50 -0800

    Bump phoenix to v1.6
    - Bump phoenix to v1.6
    - Bump phoenix live_view to v0.17
    - Bump react_live_component to 0.4.1
    - Bump slurpee
    - Bump history
    - Bump rube

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,19 @@ config :phoenix, :json_library, Jason
 # a synchronous `init` function that takes the
 # configured arguments.
 config :livebook, :default_runtime, {Livebook.Runtime.ElixirStandalone, []}
+
+# Disable livebook authentication by default
+config :livebook, :authentication_mode, :disabled
+
+# A list of custom plugs in the following format:
+#
+#    [{plug_module :: module(), opts :: keyword()}]
+#
+# The plugs are called directly before the Livebook router.
+config :livebook, :plugs, []
+
+# A list of additional notebooks to include in the Explore sections.
+#
+# Note that the notebooks are loaded and embedded in a compiled module,
+# so the paths are accessed at compile time only.
+config :livebook, :explore_notebooks, []
